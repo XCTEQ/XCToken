a584feae86d34c465cf4aa4bd507710f175463e3
Iain Mullan <iain.mullan@gmail.com>
2019-02-26 13:44:54 +0000

    cleaned up indentation

--- lib/xctoken/main.rb
+++ lib/xctoken/main.rb
@@ -6,39 +6,41 @@ require "jwt"
 require 'openssl'
 
 module XCToken
-	class Main < Thor
-	  include Thor::Actions
-  desc "generate", "Genarate AppStore Connect Token"
-  def generate
-		issuer_id = ENV["ISSUER_ID"]
-		key_id = ENV["KEY_ID"]
-		key_dir = ENV["KEY_DIR"]
+  class Main < Thor
+    include Thor::Actions
 
-		full_key_path = "#{key_dir}/AuthKey_#{key_id}.p8"
+    desc "generate", "Genarate AppStore Connect Token"
+    def generate
+      issuer_id = ENV["ISSUER_ID"]
+      key_id = ENV["KEY_ID"]
+      key_dir = ENV["KEY_DIR"]
 
-		if File.file?(full_key_path)
-			puts "Private Key Exist in the correct format with and matching identifier"
-	    else
-	       puts "Private key not found in the specified directory with key identifier assiciated"
-	       puts "Please make sure KEY_DIR and KEY_ID environmetal varibales set correctly and private key exist in the KEY_DIR"
-	       raise 'Private Key not setup correctly. Please make sure ENV[KEY_DIR] has private key'
-	    end
+      full_key_path = "#{key_dir}/AuthKey_#{key_id}.p8"
 
-	    private_key = OpenSSL::PKey.read(File.read(full_key_path))
+      if File.file?(full_key_path)
+        puts "Private Key Exist in the correct format with and matching identifier"
+      else
+         puts "Private key not found in the specified directory with key identifier assiciated"
+         puts "Please make sure KEY_DIR and KEY_ID environmetal varibales set correctly and private key exist in the KEY_DIR"
+         raise 'Private Key not setup correctly. Please make sure ENV[KEY_DIR] has private key'
+      end
 
-		$token = JWT.encode(
-		   {
-		    iss: issuer_id,
-		    exp: Time.now.to_i + 20 * 60,
-		    aud: "appstoreconnect-v1"
-		   },
-		   private_key,
-		   "ES256",
-		   header_fields = {
-		     kid: 1234,
-		   }
-		 )
-		puts $token
+      private_key = OpenSSL::PKey.read(File.read(full_key_path))
+
+      $token = JWT.encode(
+         {
+          iss: issuer_id,
+          exp: Time.now.to_i + 20 * 60,
+          aud: "appstoreconnect-v1"
+         },
+         private_key,
+         "ES256",
+         header_fields = {
+           kid: 1234,
+         }
+       )
+      puts $token
+
+    end
   end
- end
 end
